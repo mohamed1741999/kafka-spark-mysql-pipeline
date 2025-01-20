795b0cedda4332a0ad2f1bc72ef0e9310f69057f
Mohamed Gamal Hassan <81765792+mohamed1741999@users.noreply.github.com>
2025-01-20 18:25:53 +0200

    kafka_to_mysql_DAG.py

--- kafka_to_mysql_DAG.py
+++ kafka_to_mysql_DAG.py
@@ -3,24 +3,24 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'MohamedGamal',
     'depends_on_past': False,
     'start_date': datetime(2024, 1, 20),
     'retries': 1,
 }
 
 dag = DAG(
-    'run_kafka_to_mysql_notebook',
+    'kafka_to_mysql_python',
     default_args=default_args,
-    description='Run Jupyter notebook script using Airflow',
+    description='Run Python script using Airflow',
     schedule_interval='@hourly',
     catchup=False,
 )
 
-run_notebook = BashOperator(
-    task_id='run_jupyter_script',
+run_PythonTask = BashOperator(
+    task_id='run_Python_script',
     bash_command='python /home/gamal/airflow/dags/kafka_to_mysql.py',
     dag=dag,
 )
 
-run_notebook
+run_PythonTask
